f2c530e0cbb394a12ea94f8d510e758087ae4d7f
T1mS22 <tim220200.ts@gmail.com>
2022-06-05 21:56:41 +0200

    final fixed version

--- raspicontrol.py
+++ raspicontrol.py
@@ -1,19 +1,17 @@
-from telegram.ext import Updater, CommandHandler
+from telegram.ext import Updater, CommandHandler, MessageHandler
 from gpiozero import CPUTemperature
 
 
-global TOKEN
-
 with open('token.txt', 'r') as f:
-    TOKEN = f.read()
+    TOKEN = f.readline().replace('\n', '')
 
 cpu = CPUTemperature()
 
-def start(update):
-    update.message.replay_text("Hey! Welcome to RaspiControl. I'm a bot that can send you status data about your Raspberry Pi.")
+def start(update, context):
+    update.message.reply_text("Hey! Welcome to RaspiControl. I'm a bot that can send you status data about your Raspberry Pi.")
 
-def help(update):
-    update.message.replay_text("""
+def help(update, context):
+    update.message.reply_text("""
     The following commands are avaiable:
 
     /start -> Welcome Message
@@ -22,17 +20,17 @@ def help(update):
     /contact -> Information about the developer
     """)
 
-def temp(update):
-    update.message.replay_text("The current CPU temperature is: " + cpu.temperature)
+def temp(update, context):
+    update.message.reply_text(f'The current CPU temperature is: {cpu.temperature:.2f}°C')
+    
+def contact(update, context):
+    update.message.reply_text("More about Tim: https://t1ms22.github.io")
 
-def contact(update):
-    update.message.replay_text("More about Tim: https://t1ms22.github.io")
-
-updater = Updater(TOKEN, use_context=False)
+updater = Updater(TOKEN, use_context=True)
 disp = updater.dispatcher
 
 disp.add_handler(CommandHandler("start", start))
-disp.add_handler(CommandHandler("help ", help))
+disp.add_handler(CommandHandler("help", help))
 disp.add_handler(CommandHandler("temp", temp))
 disp.add_handler(CommandHandler("contact", contact))
 
